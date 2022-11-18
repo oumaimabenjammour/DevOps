c796640f306924761d8df48f69e3beea111bdce5
Hamza.Kacem <hamza.kacem@esprit.tn>
2022-11-18 01:22:49 +0100

    swagger

--- src/main/java/tn/esprit/rh/achat/util/SpringFoxSwaggerConfig.java
+++ src/main/java/tn/esprit/rh/achat/util/SpringFoxSwaggerConfig.java
@@ -43,22 +43,5 @@ public class SpringFoxSwaggerConfig {
                 .build();
     }
 
-	private ApiKey apiKey() {
-		return new ApiKey("Bearer", AUTHORIZATION_HEADER, "header");
-	}
 	
-	
-	private SecurityContext securityContext() {
-		return SecurityContext.builder()
-				.securityReferences(defaultAuth())
-				.build();
-	}
-
-	private List<SecurityReference> defaultAuth() {
-		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-		authorizationScopes[0] = authorizationScope;
-		return Arrays.asList(new SecurityReference("Bearer", authorizationScopes)); 
-
-	}
 }
